f741fbde140471bb09cb2cd39af921bec7965a5c
jatinscodingproject <152329190+jatinscodingproject@users.noreply.github.com>
2023-12-23 20:18:59 +0530

    # using app.listen instead of creating server and avoid using of http module

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 
-const http = require('http');
-
 const app = express();
 
 app.use((req,res,next) => {
     console.log('in the middleware')
-    next()
+    next()// allows the request to continue the next middleware in line. 
 });
 
 app.use((req,res,next) => {
     console.log('i am another middleware')
-    
+    res.send('<h1>hello from express</h1>')
 })
-const server = http.createServer(app)
 
-server.listen(3000)
+app.listen(3000)
