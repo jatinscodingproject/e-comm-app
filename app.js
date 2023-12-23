d2317ea91147b7287a3aa24a84b52f0e1aac73ac
jatinscodingproject <152329190+jatinscodingproject@users.noreply.github.com>
2023-12-23 20:35:17 +0530

    # handling different routes

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log('in the middleware')
-    next()// allows the request to continue the next middleware in line. 
-});
+app.use('/add-product',(req,res,next) => {
+    console.log('i am middleware')
+    res.send('<h1>"Add product page"</h1>')
+})
 
-app.use((req,res,next) => {
+app.use('/',(req,res,next) => {
     console.log('i am another middleware')
     res.send('<h1>hello from express</h1>')
 })
