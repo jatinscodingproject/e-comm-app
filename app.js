e6f2a68efffb54a4d6050a002953a74c1ccc8ef9
jatinscodingproject <152329190+jatinscodingproject@users.noreply.github.com>
2023-12-23 20:08:57 +0530

    # adding  middleware

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const http = require('http');
 const app = express();
 
 app.use((req,res,next) => {
-
+    console.log('in the middleware')
+    next()
 });
+
+app.use((req,res,next) => {
+    console.log('i am another middleware')
+    
+})
 const server = http.createServer(app)
 
 server.listen(3000)
