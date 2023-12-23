1db225fc71531eeda2e27278bd1f69bb29c273a7
jatinscodingproject <152329190+jatinscodingproject@users.noreply.github.com>
2023-12-23 21:05:06 +0530

    # sharpener challenge

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/add-product',(req,res,next) => {
-    res.send('<form action="/product" method="POST"><input type ="text" name="tilte"><button type="submit">Add Product</button></form>')
+    res.send('<form action="/product" method="POST"><input type ="text" name="tilte"><br><input type="text" name="size"><br><button type="submit">submit</button></form>')
 })
 
-app.use('/product',(req,res,next) => {
+app.post('/product',(req,res,next) => {
     console.log(req.body)
     res.redirect('/')
 })
